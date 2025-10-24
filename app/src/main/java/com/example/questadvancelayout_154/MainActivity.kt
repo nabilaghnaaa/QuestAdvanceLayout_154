e254378a96d9a6a869cec713ddb3ec005aa0458d
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-24 23:47:43 +0700

    fila MainActivity.kt

--- app/src/main/java/com/example/questadvancelayout_154/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_154/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_154Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AktivitasPertama(
+                    AktivitasKedua(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
