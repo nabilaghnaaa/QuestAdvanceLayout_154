30b01506e0b6537413b2cc506706087183825245
nabilaghnaaa <nabilaghnaaa@gmail.com>
2025-10-24 23:36:20 +0700

    buat file dan struktur dasar

---
+++ app/src/main/java/com/example/questadvancelayout_154/ui/theme/ProfileUITheme.kt
@@ -0,0 +1,21 @@
+package com.example.questadvancelayout_154.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFF1E88E5),
+    secondary = Color(0xFF0D47A1),
+    background = Color(0xFF121212)
+)
+
+@Composable
+fun ProfileUITheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        typography = MaterialTheme.typography,
+        content = content
+    )
+}
